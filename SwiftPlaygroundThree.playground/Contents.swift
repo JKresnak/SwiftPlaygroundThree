1e6d52a48dc90e38d2050496868fde3e148acbd8
Jkresnak <cmstudent@uhwo-acm-01.uhwo.hawaii.edu>
2020-02-18 10:30:19 -1000

    Problem set 1 complete Problem set 2 Almost complete

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -12,3 +12,19 @@ print("I like to eat" + name)
 }
 
 print(FruitNames[0])
+
+
+var ExpensiveCars = ["a Ferrari", "a Mercedes", "a Jaguar", "a Lambo", "an Aston Martin", "a Mclaren", "a Bugatti", "an ambulance"]
+
+    for name in ExpensiveCars
+{
+    print(name, "is a very expensive car")
+}
+
+var CarPrices: = ["$350_000", "$34_250", "$76_000", "$203_764", "$250_000", "$200_000", "$19 million", "A saved life is priceless"]
+    
+    {
+    print("name + 100")
+}
+
+//I can't figure out the integer part
